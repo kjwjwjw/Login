c43467526016a4c925b9b6e50dc5c0854338d49a
tbs03 <tbs03112@naver.com>
2024-03-10 16:21:44 +0900

    committ

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -2,13 +2,19 @@ package com.codingrecipe.member.controller;
 
 
 import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
 
     @GetMapping("/member/save")
     public String saveForm() {
@@ -19,6 +25,8 @@ public class MemberController {
     @PostMapping("/member/save")
     public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("memberDTO = " + memberDTO);
+
+        memberService.save(memberDTO);
 
         return "index";
 
